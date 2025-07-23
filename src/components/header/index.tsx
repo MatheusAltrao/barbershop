30d697d94eab4f4fcd51a21ba43c60705cf673f0
Matheus Altrao <matheusaltrao2@gmail.com>
2025-07-23 00:57:53 -0400

    ✨ feat(header): Adiciona autenticação com Google e exibe informações do usuário no `Header`

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,3 +1,5 @@
+"use server";
+import GoogleIcon from "@/assets/icons/google.svg";
 import {
   Sheet,
   SheetContent,
@@ -6,12 +8,18 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import { auth, signIn, signOut } from "@/lib/auth";
 import { Calendar, DoorOpen, Home, Menu } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 import { Button } from "../ui/button";
 import { Separator } from "../ui/separator";
 
-export default function Header() {
+export default async function Header() {
+  const session = await auth();
+
+  const isLoggedIn = !!session?.user;
+
   const links = [
     {
       label: "Início",
@@ -43,37 +51,73 @@ export default function Header() {
 
               <Separator className="my-4" />
 
-              <div className="flex items-center gap-2">
-                <div className="h-12 min-w-12 rounded-full bg-primary"></div>
+              {isLoggedIn && (
+                <div className="flex items-center gap-2">
+                  <Image
+                    className=" rounded-full "
+                    src={session.user?.image || ""}
+                    alt={session.user?.name || ""}
+                    width={48}
+                    height={48}
+                  />
 
-                <div className="flex flex-col ">
-                  <span className="font-bold">Pedro Gonçalves</span>
-                  <span className="text-xs text-muted-foreground">
-                    pedrogoncalves@gmail.com
-                  </span>
+                  <div className="flex flex-col ">
+                    <span className="font-bold">{session.user?.name}</span>
+                    <span className="text-xs text-muted-foreground">
+                      {session.user?.email}
+                    </span>
+                  </div>
                 </div>
-              </div>
+              )}
 
-              <Separator className="my-4" />
+              {!isLoggedIn && (
+                <form
+                  action={async () => {
+                    "use server";
+                    await signIn("google");
+                  }}
+                >
+                  <Button className="gap-2 w-full" variant={"secondary"}>
+                    <Image
+                      width={24}
+                      height={24}
+                      src={GoogleIcon}
+                      alt="Google"
+                    />
+                    Entrar com o Google
+                  </Button>
+                </form>
+              )}
 
-              <div className="flex flex-col flex-1 justify-between  ">
-                <div className="flex flex-col gap-2">
-                  {links.map((link) => (
-                    <Link className="w-full" key={link.href} href={link.href}>
-                      <Button
-                        className="w-full justify-start"
-                        variant={"outline"}
-                      >
-                        {link.icon} {link.label}
-                      </Button>
-                    </Link>
-                  ))}
+              {isLoggedIn && <Separator className="my-4" />}
+
+              {isLoggedIn && (
+                <div className="flex flex-col flex-1 justify-between  ">
+                  <div className="flex flex-col gap-2">
+                    {links.map((link) => (
+                      <Link className="w-full" key={link.href} href={link.href}>
+                        <Button
+                          className="w-full justify-start"
+                          variant={"outline"}
+                        >
+                          {link.icon} {link.label}
+                        </Button>
+                      </Link>
+                    ))}
+                  </div>
+
+                  <form
+                    action={async () => {
+                      "use server";
+                      await signOut();
+                    }}
+                  >
+                    <Button className="w-full " variant={"destructive"}>
+                      <DoorOpen /> Sair da conta
+                    </Button>
+                  </form>
                 </div>
-
-                <Button className="w-full " variant={"destructive"}>
-                  <DoorOpen /> Sair da conta
-                </Button>
-              </div>
+              )}
             </div>
           </SheetContent>
         </Sheet>
