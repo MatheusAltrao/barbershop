3fd29cb48b8c0e07d67f039b0dff0becf07a05fe
Matheus Altrao <matheusaltrao2@gmail.com>
2025-07-28 22:16:45 -0400

    ✨ feat(services): Adiciona lógica de autenticação para exibir botão de reserva ou agendamento de serviço

--- src/components/sections/services/service-section.tsx
+++ src/components/sections/services/service-section.tsx
@@ -1,11 +1,17 @@
 "use server";
+import { signInAction } from "@/actions/login/signIn.action";
 import { getServicesAction } from "@/actions/services/get-services.action";
 import TitleSection from "@/components/typography/title-section";
+import { Button } from "@/components/ui/button";
+import { auth } from "@/lib/auth";
 import ServiceCard from "./service-card";
 import ServiceCardSchedule from "./service-card-schedule";
 
 export default async function ServiceSection() {
   const services = await getServicesAction();
+  const session = await auth();
+
+  const isLoggedIn = !!session?.user;
 
   return (
     <div className="px-5 ">
@@ -14,10 +20,18 @@ export default async function ServiceSection() {
       <div className="space-y-2">
         {services.map((service) => (
           <ServiceCard key={service.title} service={service}>
-            <ServiceCardSchedule
-              services={services}
-              selectedService={service}
-            />
+            {!isLoggedIn && (
+              <form action={signInAction}>
+                <Button size={"sm"}>Reservar</Button>
+              </form>
+            )}
+
+            {isLoggedIn && (
+              <ServiceCardSchedule
+                services={services}
+                selectedService={service}
+              />
+            )}
           </ServiceCard>
         ))}
       </div>
